1e666cba033645dea06fe32be369620ad0c2bbd8
LucasSisti <150533666+LucasSisti@users.noreply.github.com>
2023-11-23 22:11:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const ejs = require('ejs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,21 +8,17 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Configurar o uso de views (usando EJS como exemplo)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // Função para validar dados do formulário
 function validarDados(dados) {
     const erros = {};
 
-    // Adicione validações conforme necessário
+    // Adicione suas regras de validação aqui
     // Exemplo: Verificar se o campo nome está preenchido
     if (!dados.txtnome) {
         erros.nome = 'Campo obrigatório.';
     }
 
-    // Adicione outras validações aqui...
+    // Adicione mais verificações conforme necessário
 
     return erros; // Retorna o objeto de erros
 }
@@ -35,22 +30,37 @@ app.post('/cadastro', (req, res) => {
     const errosValidacao = validarDados(dados);
 
     if (Object.keys(errosValidacao).length > 0) {
+        // Se houver erros, renderize o formulário novamente com mensagens de erro
         return res.render('formulario', {
             mensagemErroNome: errosValidacao.nome,
-            mensagemErroDataNascimento: errosValidacao.dataNascimento,
             mensagemErroEndereco: errosValidacao.endereco,
             mensagemErroEmail: errosValidacao.email,
-            mensagemErroSexo: errosValidacao.sexo,
+            mensagemErroDataNascimento: errosValidacao.dataNascimento,
             mensagemErroSenha: errosValidacao.senha,
             mensagemErroSenhaConfirmacao: errosValidacao.senhaConfirmacao,
+            mensagemErroSexo: errosValidacao.sexo,
             mensagemErroAnimais: errosValidacao.animais,
+            // Passe os dados do formulário de volta para o formulário para manter os valores preenchidos
             dadosFormulario: dados,
         });
     }
 
-    // Se não houver erros, continue com o processamento dos dados
+    // Se não houver erros, você pode prosseguir com o processamento dos dados
     console.log('Dados do formulário:', dados);
+    
+    // Aqui você pode adicionar lógica adicional para processar os dados
+
+    // Em vez de enviar 'Formulário recebido com sucesso!', você pode redirecionar ou enviar uma resposta adequada
     res.send('Formulário recebido com sucesso!');
+});
+
+// Configurando o mecanismo de visualização EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Rota para renderizar o formulário inicial
+app.get('/', (req, res) => {
+    res.render('formulario', { mensagemErroNome: '' });
 });
 
 app.listen(port, () => {
